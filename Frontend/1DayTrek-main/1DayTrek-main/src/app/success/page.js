c26aed874ccc347fc83169a4acfb30254cc5b9fd
Komal-2108 <thekomal2502@gmail.com>
2025-11-05 16:00:02 +0530

    Success page edit

---
+++ Frontend/1DayTrek-main/1DayTrek-main/src/app/success/page.js
@@ -0,0 +1,10 @@
+// app/success/page.js
+
+export default function SuccessPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Payment Successful! 🎉</h1>
+      <p>Thank you for your order.</p>
+    </div>
+  );
+}
